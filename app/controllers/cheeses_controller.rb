8b9c5e353ff60967c4a23cc59e8e9379389eb077
Judy Mwai <juddiemukami@gmail.com>
2022-10-02 12:16:23 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,24 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    # To include that summary method in the JSON response, we can do so using the methods
+    render json: cheeses, only: [:created_at, :updated_at], methods: [:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+
+    if cheese
+
+    # producing only id, name, price, and is_best_seller for each cheese
+    render json: cheese, except: [:created_at, :updated_at]
+     else 
+      render json: {error: 'cheese not found'}, status: :not_found
+
+    end
   end
 
 end
+
+# NB Both the only and except options are available to us thanks to the .as_json
